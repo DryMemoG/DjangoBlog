a7c34c3a3c674545c4f802033d31fcb31a90ff80
Guillermo Galindo <31331135+DryMemoG@users.noreply.github.com>
2020-10-25 00:56:59 -0600

    Nuevo Commit
    Hasta aquí llevo solamente la parte del primer despliegue

---
+++ mientorno/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\dryme\Documents\GitHub\DjangoBlog\mientorno\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
