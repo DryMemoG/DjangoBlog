82d11488a6d8290310cdb5fe6d7beaa947230d99
Guillermo Galindo <31331135+DryMemoG@users.noreply.github.com>
2020-10-24 19:31:15 -0600

    Borre todo  xd porque lo haré de nuevo en el güindous xdxdxd

--- blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Publicacion
-
-# Create your views here.
-def publicaciones(request):
-	publicacion = Publicacion.objects.all()
-	return render(request, 'blog/publicaciones.html',{'publicaciones':publicacion})
